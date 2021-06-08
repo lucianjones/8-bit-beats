6b1ba7c8cee5b1c1d1a1ee53fa5c97450735e792
Lucian Jones <lucianjones96@gmail.com>
2021-06-08 16:38:22 -0400

    “”

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -36,7 +36,7 @@ function Navigation({ isLoaded }){
         <div>
           <LoginFormModal />
           <SignupFormModal />
-          <button onClick={demo}>Demo User</button>
+          <button className='btn yellow' onClick={demo}>Demo User</button>
         </div>
       </>
     );
